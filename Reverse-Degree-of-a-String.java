391c1cfee0ceb21cf52df5d38237528183b39074
POOJAGUPTA112000 <poojaguptargpv11072000@gmail.com>
2025-11-03 21:11:35 +0530

    [3498] [Time Beats: 5.13%] [Memory Beats: 5.49%] - LeetPush

---
+++ Reverse-Degree-of-a-String.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverseDegree(String s){
+        int pro=97;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=26;i>=1;i--){
+            map.put((char)pro,i);
+            pro++;
+        }
+        System.out.println(map);
+        pro=0;
+        for(int i=0;i<=s.length()-1;i++){
+            pro+=(map.get(s.charAt(i))*(i+1));
+        }
+        return pro;
+    }
+}
