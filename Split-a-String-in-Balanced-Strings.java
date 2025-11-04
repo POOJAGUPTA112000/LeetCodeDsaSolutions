017ae9473de7fed690958caa3a9efb121fbba2d2
POOJAGUPTA112000 <poojaguptargpv11072000@gmail.com>
2025-11-04 16:57:21 +0530

    [1221] [Time Beats: 100.00%] [Memory Beats: 6.17%] - LeetPush

---
+++ Split-a-String-in-Balanced-Strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int bal=0;
+        int ans=0;
+        for( char c : s.toCharArray()){
+            if(c=='L'){
+                bal++;
+            }else{
+                bal--;
+            }
+            if(bal==0){
+               ans++; 
+            }
+        }
+        return ans;
+    }
+}
