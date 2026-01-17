3ab888c7a1c803f06b7c6db1c435b63007b37049
POOJAGUPTA112000 <poojaguptargpv11072000@gmail.com>
2026-01-17 22:49:05 +0530

    [2078] [Time Beats: 10.89%] [Memory Beats: 12.98%] - LeetPush

--- Two-Furthest-Houses-With-Different-Colors.java
+++ Two-Furthest-Houses-With-Different-Colors.java
@@ -1,8 +1,8 @@
 1class Solution {
 2    public int maxDistance(int[] colors) {
 3        int m=0;
-4        for( int i=0;i<colors.length-1;i++){
-5            for( int j=i+1;j<=colors.length-1;j++){
+4        for( int i=0;i<colors.length-1/2;i++){
+5            for( int j=colors.length-1;j>=colors.length/2;j--){
 6                if(colors[i]!=colors[j]){
 7                    m = Math.max(m, Math.abs(i-j));
 8                }
