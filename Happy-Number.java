5d64d79afa59e98f4fbf021b85c50832489db735
POOJAGUPTA112000 <poojaguptargpv11072000@gmail.com>
2026-01-30 19:43:56 +0530

    [202] [Time Beats: 76.51%] [Memory Beats: 58.42%] - LeetPush

--- Happy-Number.java
+++ Happy-Number.java
@@ -1,28 +1,22 @@
-1class Solution{
+1class Solution {
 2    public boolean isHappy(int n) {
-3        if(n==1){
-4            return true;
-5        }
-6        // if(n<10){
-7        //     return false;
-8        // }
-9        HashMap<Integer,Integer> h =new HashMap<>();
-10        while(n!=1){
-11            int num=n;
-12            int sum=0;
-13            while(num!=0){
-14                sum+=Math.pow((num%10),2);
-15                // System.out.println(sum);
-16                num=num/10;
-17            }
-18            if(h.containsKey(sum)){
-19                return false;
-20            }else{
-21                h.put(sum,h.getOrDefault(sum,0)+1);
-22            }
-23            System.out.println(sum);
-24            n=sum;
-25        }
-26        return true;
-27    }
-28}
+3        int slow=n;
+4        int fast=n;
+5        do{
+6            slow=sumSq(slow);
+7            fast=sumSq(sumSq(fast));
+8        }while(slow!=fast);
+9        if(slow==1){
+10            return true;
+11        }
+12        return false;
+13    }
+14    public int sumSq( int num){
+15        int sum=0;
+16        while(num!=0){
+17            sum+=Math.pow(num%10,2);
+18            num=num/10;
+19        }
+20        return sum;
+21    }    
+22}
