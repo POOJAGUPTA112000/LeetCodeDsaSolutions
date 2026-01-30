fd0e8d0f63d48f41240c4a17b7aa677d8041369e
POOJAGUPTA112000 <poojaguptargpv11072000@gmail.com>
2026-01-30 19:45:27 +0530

    [876] [Time Beats: 100.00%] [Memory Beats: 65.44%] - LeetPush

--- Middle-of-the-Linked-List.java
+++ Middle-of-the-Linked-List.java
@@ -10,28 +10,88 @@
 10 */
 11class Solution {
 12    public ListNode middleNode(ListNode head) {
-13        return findSize(head);
-14        
-15    }
-16   
-17    public ListNode findSize(ListNode head){
-18        int count=1;
-19        ListNode temp = head;
-20        while(temp!=null){
-21            temp=temp.next;
-22            count++;
-23        }
-24        if(count%2==0){
-25            count= count/2;
-26        }else{
-27        count=(count/2)+1;
-28        }
-29        temp=head;
-30        while(count>1){
-31            temp=temp.next;
-32            count--;
-33        }
-34        return temp;
-35    }
+13        if(head==null){
+14            return head;
+15        }
+16        ListNode slow=head;
+17        ListNode fast=head;
+18        while(fast!=null && fast.next!=null){
+19            fast=fast.next.next;
+20            slow=slow.next;
+21        }
+22        return slow;
+23    }
+24}
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
 36
-37}
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61/**
+62//  * Definition for singly-linked list.
+63//  * public class ListNode {
+64//  *     int val;
+65//  *     ListNode next;
+66//  *     ListNode() {}
+67//  *     ListNode(int val) { this.val = val; }
+68//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+69//  * }
+70//  */
+71// class Solution {
+72//     public ListNode middleNode(ListNode head) {
+73//         return findSize(head);
+74        
+75//     }
+76   
+77//     public ListNode findSize(ListNode head){
+78//         int count=1;
+79//         ListNode temp = head;
+80//         while(temp!=null){
+81//             temp=temp.next;
+82//             count++;
+83//         }
+84//         if(count%2==0){
+85//             count= count/2;
+86//         }else{
+87//         count=(count/2)+1;
+88//         }
+89//         temp=head;
+90//         while(count>1){
+91//             temp=temp.next;
+92//             count--;
+93//         }
+94//         return temp;
+95//     }
+96
+97// }
