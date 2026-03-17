4058ceef07f7879b2688814c3cbb2c304883eaac
POOJAGUPTA112000 <poojaguptargpv11072000@gmail.com>
2026-03-17 20:33:21 +0530

    [387] [Time Beats: 21.49%] [Memory Beats: 95.75%] - LeetPush

--- First-Unique-Character-in-a-String.java
+++ First-Unique-Character-in-a-String.java
@@ -1,21 +1,37 @@
-class Solution {
-    public int firstUniqChar(String s) {
-        LinkedHashMap<Character,Integer> map = new LinkedHashMap<>();
-        for(int i=0;i<=s.length()-1;i++){
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-        }
-        // char ch;
-        // for(Map.Entry<Character,Integer> list : map.entrySet()){
-        //     if(list.getValue()==1){
-        //         ch = list.getKey();
-        //         break;
-        //     }
-        // }
-        for( int i=0;i<=s.length()-1;i++){
-            if(map.get(s.charAt(i))==1){
-                return i;
-            }
-        }
-        return -1;
-    }
-}
+1class Solution {
+2    public int firstUniqChar(String s) {
+3        LinkedHashMap<Character,Integer> map =new LinkedHashMap<>();
+4        for( char c :  s.toCharArray()){
+5            map.put(c,map.getOrDefault(c,0)+1);
+6        }
+7        for(int i=0;i<=s.length()-1;i++){
+8            if(map.get(s.charAt(i))==1){
+9                return i;
+10            }
+11        }
+12        return -1;
+13    }
+14}
+15
+16
+17// class Solution {
+18//     public int firstUniqChar(String s) {
+19//         LinkedHashMap<Character,Integer> map = new LinkedHashMap<>();
+20//         for(int i=0;i<=s.length()-1;i++){
+21//             map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+22//         }
+23//         // char ch;
+24//         // for(Map.Entry<Character,Integer> list : map.entrySet()){
+25//         //     if(list.getValue()==1){
+26//         //         ch = list.getKey();
+27//         //         break;
+28//         //     }
+29//         // }
+30//         for( int i=0;i<=s.length()-1;i++){
+31//             if(map.get(s.charAt(i))==1){
+32//                 return i;
+33//             }
+34//         }
+35//         return -1;
+36//     }
+37// }
