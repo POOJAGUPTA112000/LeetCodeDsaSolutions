21094aede35bfee0544440a60328edf10263ebfb
POOJAGUPTA112000 <poojaguptargpv11072000@gmail.com>
2026-04-09 23:49:26 +0530

    [1356] [Time Beats: 34.08%] [Memory Beats: 22.23%] - LeetPush

--- Sort-Integers-by-The-Number-of-1-Bits.java
+++ Sort-Integers-by-The-Number-of-1-Bits.java
@@ -4,18 +4,18 @@
 4        for( int i : arr){
 5            map.computeIfAbsent(bin(i),k->new ArrayList()).add(i);           
 6        }
-7        int [] newa = new int [arr.length];
+7        // int [] newa = new int [arr.length];
 8        int j=0;
-9        System.out.println(map);
+9        // System.out.println(map);
 10        for(Map.Entry<Integer,ArrayList<Integer>> entry : map.entrySet()){
 11            ArrayList<Integer> l1 = entry.getValue();
 12            Collections.sort(l1);
 13            for(int i=0;i<=l1.size()-1;i++){
-14                newa[j]=l1.get(i);
+14                arr[j]=l1.get(i);
 15                j++;
 16            }
 17        }
-18        return newa;         
+18        return arr;         
 19    }
 20    public int bin(int i){
 21        int count=0;
